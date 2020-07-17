546b677ccefc8813bf08fc4343b7381e4a8e558a
Michael Cuddy <pryankster@hotmail.com>
2020-07-16 22:23:13 -0700

    initial commit

---
+++ Assets/Scripts/TumbleCube.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TumbleCube : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public bool random = true;
+    public Vector3 rotate;
+
+    private void Awake()
+    {
+        if (random) rotate = new Vector3(Random.Range(-180f, 180f), Random.Range(-180f, 180f), Random.Range(-180f, 180f));
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotate * Time.deltaTime, Space.Self);
+    }
+}
